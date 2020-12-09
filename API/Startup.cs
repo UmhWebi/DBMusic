7ee74111c87d2602a2966a430d6579f1ed628213
Tito Venter <umhwebi@venter.com>
2020-12-09 15:40:47 -0500

    Added Angular and CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -36,6 +36,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -55,7 +56,7 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));  // Must come after Routing and before Authorization
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
