f6fc450e977d7d6a8bb16da7ce281298309a9f72
Tito Venter <umhwebi@venter.com>
2020-12-11 15:11:31 -0500

    Client login and register

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { NgModule } from '@angular/core';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent, 
+    NavComponent, HomeComponent, RegisterComponent],
+  imports: [
+    BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule, 
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule.forRoot()],
+    
   providers: [],
   bootstrap: [AppComponent],
 })
